3955be4135b97062ee7cc48e1671bbb406caea6c
believeinlain <saaelmore@gmail.com>
2021-02-27 13:51:24 -1000

    Updated redirect

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 });
 
 // set up a route to redirect https to http
-http.get('*', function(req, res) {  
+app.use('*', function(req, res) {  
   res.redirect('http://' + req.headers.host + req.url);
 });
 
