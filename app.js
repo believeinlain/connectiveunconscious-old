f385aa642fbe6b8fa01a3a4ea4959d8033c3cdda
Stephan Aelmore <saelmore@connectiveunconscious.com>
2021-02-28 00:23:09 +0000

    Messed stuff up a bit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 });
 
 // set up a route to redirect https to http
-app.use('*', function(req, res) {  
+app.get('*', function(req, res) {  
   res.redirect('http://' + req.headers.host + req.url);
 });
 
